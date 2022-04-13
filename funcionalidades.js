e14095ba870088b5a04dc8bde7434fd66642c771
LeoNR <leonardo.rigonini@gmail.com>
2022-04-13 11:25:54 -0300

    commit

---
+++ funcionalidades.js
@@ -0,0 +1,18 @@
+document.querySelector('#add').onclick = function() {
+    var minhaTarefa = document.querySelector('#tarefa').value
+
+    if(minhaTarefa.length == 0) {
+        alert("Por favor, Digite sua tarefa!")
+        return
+    }
+   
+    document.querySelector('#tarefas').innerHTML += `
+    <div class="tarefa">
+        <span>${minhaTarefa}</span>
+    </div>
+    `;
+
+document.querySelector('#tarefa').value= ''
+
+
+}
